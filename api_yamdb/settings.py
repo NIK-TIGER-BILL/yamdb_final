0786592fa23d7344a45016823d39a98a889d15e3
Nikita Notebook <nik.tigr2010@gmail.com>
2021-09-15 22:50:18 +0300

    Ну попробуем так! (удалил запуск локальный тестов django)

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -97,9 +97,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
