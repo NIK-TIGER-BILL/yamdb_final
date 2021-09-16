b5eacb2c7730dd073b0a814c414a2e4679bfbce9
NIK-TIGER-BILL <nik.tigr2010@gmail.com>
2021-09-16 16:20:26 +0300

    add telegram messages

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
