085ee55f41c7145c8840b84913d15f8f1bca7a05
NIK-TIGER-BILL <nik.tigr2010@gmail.com>
2021-09-16 16:21:47 +0300

    add telegram messages

--- tests/test_yamdb_yml.py
+++ tests/test_yamdb_yml.py
@@ -8,19 +8,19 @@ class TestWorkflow:
 
     def test_workflow(self):
         try:
-            with open(f'{os.path.join(settings.BASE_DIR, "yamdb_workflow.yaml")}', 'r') as f:
+            with open(f'{os.path.join(settings.BASE_DIR, "yamdb_workflow.yml")}', 'r') as f:
                 yamdb = f.read()
         except FileNotFoundError:
-            assert False, 'Проверьте, что добавили файл yamdb_workflow.yaml в корневой каталог для проверки'
+            assert False, 'Проверьте, что добавили файл yamdb_workflow.yml в корневой каталог для проверки'
 
         assert (
             re.search(r'on:\s*push:\s*branches:\s*-\smaster', yamdb) or
             'on: [push]' in yamdb or
             'on: push' in yamdb 
-        ), 'Проверьте, что добавили действие при пуше в файл yamdb_workflow.yaml' 
-        assert 'pytest' in yamdb, 'Проверьте, что добавили pytest в файл yamdb_workflow.yaml'
-        assert 'appleboy/ssh-action' in yamdb, 'Проверьте, что добавили деплой в файл yamdb_workflow.yaml'
+        ), 'Проверьте, что добавили действие при пуше в файл yamdb_workflow.yml'
+        assert 'pytest' in yamdb, 'Проверьте, что добавили pytest в файл yamdb_workflow.yml'
+        assert 'appleboy/ssh-action' in yamdb, 'Проверьте, что добавили деплой в файл yamdb_workflow.yml'
         assert 'appleboy/telegram-action' in yamdb, (
             'Проверьте, что добавили доставку отправку telegram сообщения '
-            'в файл yamdb_workflow.yaml'
+            'в файл yamdb_workflow.yml'
         )
